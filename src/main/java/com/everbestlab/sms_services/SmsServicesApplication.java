8df1562c80e9781f3bdc3777dd921de380348959
Ulug'bek <ulugbekrozimboyev@gmail.com>
2024-09-10 11:57:56 +0500

    init project

---
+++ src/main/java/com/everbestlab/sms_services/SmsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.everbestlab.sms_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsServicesApplication.class, args);
+	}
+
+}
